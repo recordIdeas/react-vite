956958a2840b23646dac0566408ade4236be90b6
recordideas-react <2998675439@qq.com>
2024-08-12 14:29:52 +0800

    Updates

---
+++ assets/index-Cm4CX1zp.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CVYQh7d4.js";function o(){return t.jsx("h2",{children:"Welcome to the contact page"})}export{o as default};
---
+++ assets/newUser-tZTAVbQU.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CVYQh7d4.js";function t(){return e.jsx("h2",{children:"Welcome to Create User"})}export{t as default};
