078e1ae447529dc406be00bddeea957d65fc8383
recordideas-react <2998675439@qq.com>
2024-10-25 14:56:17 +0800

    Updates

---
+++ assets/index-BryoLYSa.js
@@ -0,0 +1,1 @@
+import{j as t}from"./main-DvwCiDGV.js";const o=()=>t.jsx(t.Fragment,{children:t.jsx("h2",{children:"Welcome to the contact page"})});export{o as default};
--- assets/index-mNG75tz1.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./main-DVNbQ5FE.js";const o=()=>t.jsx(t.Fragment,{children:t.jsx("h2",{children:"Welcome to the contact page"})});export{o as default};
